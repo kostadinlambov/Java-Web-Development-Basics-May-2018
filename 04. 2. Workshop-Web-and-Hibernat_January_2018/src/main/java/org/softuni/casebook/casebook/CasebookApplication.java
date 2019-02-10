59ad82b4eb4b99f5cf317d38cc9cac8b474dad87
Kostadin Lambov <valchak@abv.bg>
2019-02-10 18:53:51 +0100

    Added resources for Workshop-Web-and-Hibernate II

---
+++ 04. 2. Workshop-Web-and-Hibernat_January_2018/src/main/java/org/softuni/casebook/casebook/CasebookApplication.java
@@ -0,0 +1,12 @@
+package org.softuni.casebook.casebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasebookApplication.class, args);
+    }
+}
